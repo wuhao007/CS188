7adf2692d3f2a150f10430053c25648540fd37ba
Hao Wu <hao.wu@HaowuSE-MBP.local>
2015-02-26 08:19:27 -0800

    first homework

--- tutorial/buyLotsOfFruit.py
+++ tutorial/buyLotsOfFruit.py
@@ -34,6 +34,8 @@ def buyLotsOfFruit(orderList):
     """
     totalCost = 0.0
     "*** YOUR CODE HERE ***"
+    for (fruit, numPounds) in orderList:
+        totalCost += fruitPrices[fruit] * numPounds
     return totalCost
 
 # Main Method
--- tutorial/shopSmart.py
+++ tutorial/shopSmart.py
@@ -29,7 +29,14 @@ def shopSmart(orderList, fruitShops):
         fruitShops: List of FruitShops
     """
     "*** YOUR CODE HERE ***"
-    return None
+    bestShop = None
+    mx = float('inf')
+    for shop in fruitShops:
+        totalCost = shop.getPriceOfOrder(orderList) 
+        if totalCost < mx:
+            mx = totalCost
+            bestShop = shop
+    return bestShop
 
 if __name__ == '__main__':
   "This code runs when you invoke the script from the command line"
